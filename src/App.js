66b2d65c5cd060362222b524d38504a78a2a7290
Kristal Shrestha <kristalstha20@gmail.com>
2025-07-23 23:13:06 +0545

    this is my first time in react

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h2>kristal Shrestha</h2>
+        <p>Kristalstha2@gmail.com &nbsp;&nbsp;&nbsp;&nbsp;+977 0979887 &nbsp;&nbsp;<a href="https://github.com/Kristal0024" target="_blank">Kristal0024</a>
+        <a href="https://www.linkedin.com/in/kristal-shrestha-040a0134b/">LinkedIn</a></p>
+        <p>I am passionate about web development, i love to create web designes. I am currently pursuing bachelors. I am proficient in css and i am constantly learning new frontend technologies to imorove my skills.</p>
+        <div class="">
+            <h3>Education</h3>
+            <ul>
+                <li>Tribhuvan University <b>(Bim)-2023 batch</b></li>
+                <li>Millenium Secondary school <b>(+2)-2022 batch</b></li>
+            </ul>
+        </div>
+        <div class="">
+            <h3>Skills</h3>
+            <ul>
+                <li>HTML</li>
+                <li>css</li>
+                <ul>
+                    <li>Tailwind css</li>
+                    <li>scss</li>
+                </ul>
+                <li>javascript</li>
+                <li>wordpress</li>
+                <li>web framework</li>
+                <ul>
+                    <li>React Js</li>
+                    <li>next js</li>
+                    <li>Bootstrap</li>
+                </ul>
+                </ul>
+        </div>
+        <div class="">
+            <h3>Experiance</h3>
+            <ul>
+                <li>Frontend developer at xyz company</li>
+            </ul>
+        </div>
+        </>
+  );
+}
+
+export default App;
