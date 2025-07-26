fb838e077cc31b349a1e51196d28e57c1ebdfdad
Kristal Shrestha <kristalstha20@gmail.com>
2025-07-26 20:56:32 +0545

    Additional sections

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ function App() {
         <main>
         <div className="landing">
               <div className="details">
-            <div><h3>Hello, I'm</h3></div>
-            <div><h3>Kristal Shrestha</h3></div>
-            <div><p>A Creative Designer From Nepal</p></div>
-            <div><p>I'm Creative Designer based in Nepal, and i'm very passionate and dedicated to my work.</p></div>
-            <div><button>About Me</button>&nbsp;&nbsp;<a href="">Facebook</a>&nbsp;&nbsp;<a href="">linkedin</a>&nbsp;&nbsp;<a href="">GitHub</a></div>
+             <h3><span className='color'>Hello, I'm</span><br />
+            Kristal Shrestha</h3>
+            <p>A Creative Designer From Nepal <br />
+            I'm Creative Designer based in Nepal, and i'm very passionate and dedicated to my work.</p>
+            <button className='Aboutbtn'>About Me</button>&nbsp;&nbsp;<a href="">Facebook</a>&nbsp;&nbsp;<a href="">linkedin</a>&nbsp;&nbsp;<a href="">GitHub</a>
             </div>
             <div className="avatar">
-              <img src="/pic.png" alt="This is my picture" />
+              <img src="" alt="picture" />
               <img src="/html.png" alt="" />
               <img src="/css.png" alt="" />
               <img src="/js.png" alt="" />
@@ -63,7 +63,7 @@ function App() {
           </div>
           <div className="right">
             <div className="title">
-              <span>I'm a designer</span>
+              <span className='color'>I'm a designer</span>
               <h3>I can Design Anything You Want</h3>
             </div>
             <div className="">
@@ -74,6 +74,172 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="">
+         <div className="">
+           <p className='color'>Portfolio</p>
+          <h3>My Amazing Works</h3>
+          <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design</p>
+         </div>
+         <div className="">
+          <ul>
+            <li>All</li>
+            <li>Youtube</li>
+            <li>popup</li>
+            <li>Details</li>
+          </ul>
+         </div>
+         <div className="">
+          <img src="" alt="Mockup Shape" />
+          <img src="" alt="Magic art" />
+          <img src="" alt="Art System" />
+         </div>
+        </div>
+        <div className="">
+          <div className="left">
+            <p className='color'>Design is Life</p>
+            <h3>I Develop Skills Regularly to Keep Me Update</h3>
+            <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design</p>
+            <div className="">
+              <span>Html</span><br />
+              <progress value="70" max="100"></progress>
+              <br />
+              <span>CSS</span><br />
+              <progress value="60" max="100"></progress><br />
+              <span>Javascript</span><br />
+              <progress value="60" max="100"></progress>
+              </div>
+          </div>
+          <div className="right">
+            <img src="" alt="here is an image" />
+          </div>
+        </div>
+           <div className="">
+         <div>
+            <p className='color'>Services</p>
+          <h3>What i Do For Clients</h3>
+          <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design</p>
+         </div>
+         <div className="">
+          <ul>
+            <li>
+              <img src="" alt="Icon" />
+              <div className="">
+                <h3>Creative Design</h3>
+                <span>Starts from <span>$99</span></span>
+                <p>Web design refers to the design of websites that are displayed on the internet. It usually refers to the user experience aspects of website development</p>
+              </div>
+            </li>
+             <li>
+              <img src="" alt="Icon" />
+              <div className="">
+                <h3>Graphic Design</h3>
+                <span>Starts from <span>$99</span></span>
+                <p>Web design refers to the design of websites that are displayed on the internet. It usually refers to the user experience aspects of website development</p>
+              </div>
+            </li>
+             <li>
+              <img src="" alt="Icon" />
+              <div className="">
+                <h3>UI/UX Design</h3>
+                <span>Starts from <span>$99</span></span>
+                <p>Web design refers to the design of websites that are displayed on the internet. It usually refers to the user experience aspects of website development</p>
+              </div>
+            </li>
+             <li>
+              <img src="" alt="Icon" />
+              <div className="">
+                <h3>Web Design</h3>
+                <span>Starts from <span>$99</span></span>
+                <p>Web design refers to the design of websites that are displayed on the internet. It usually refers to the user experience aspects of website development</p>
+              </div>
+            </li>
+          </ul>
+         </div>
+        </div>
+        <div className="">
+        <div>
+            <p className='color'>Testimonials</p>
+          <h3>What My clients say</h3>
+          <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design</p>
+         </div>
+        </div>
+        <div className="Blog">
+           <div>
+            <p className='color'>From my Blog</p>
+          <h3>Our Recent Updated, Blog, Tips, Tricks & More</h3>
+         </div>
+         <div className="">
+        <div className="">
+          <img src="" alt="image" />
+          <h3>Web Development</h3>
+          <p>
+            Jim Morisson Says when the musics over turn off the light</p>
+        </div>
+         <div className="">
+          <img src="" alt="image" />
+          <h3>Branding</h3>
+          <p>How to be appreciated for your hard work as a developer</p>
+        </div>
+         <div className="">
+          <img src="" alt="image" />
+          <h3>Social Media</h3>
+          <p>How designers and developers can collaborate better</p>
+        </div>
+         </div>
+        </div>
+        <div className="">
+          <div className="">
+            <h3>Subscribe Now</h3>
+            <h2>Get My NewsLetter</h2>
+            <p>Get latest news, updates, tips and trics in your inbox</p>
+          </div>
+          <div className="">
+            <input type="email" name="email" placeholder='Your Email Here' /><button>Send Now</button>
+          </div>
+        </div>
+        <div className="contact">
+            <div>
+            <p className='color'>Contact Me</p>
+          <h3>I Want to Hear From You</h3>
+          <p>Please fill out the form on this section to contact with me. Or call between 9:00 a.m. and 8:00 p.m. ET, Monday through Friday</p>
+         </div>
+         <div className="">
+          <div className="">
+            <span>
+              <img src="" alt="LOGO" />
+              <h3>Address</h3>
+              <p>20, Somewere in the World</p>
+            </span>
+             <span>
+              <img src="" alt="LOGO" />
+              <h3>Email</h3>
+              <a href="Kristalstha20@gmail.com">Kristalstha20@gmail.com</a>
+            </span>
+             <span>
+              <img src="" alt="LOGO" />
+              <h3>Phone</h3>
+              <p>+977 9800000</p>
+            </span>
+          </div>
+          <div className="">
+            <form action="">
+              <input type="text" placeholder='Your Name'/>
+              <input type="email" placeholder='Your Email'/><br />
+              <input type="number" placeholder='Your Phone'/>
+              <input type="text" placeholder='Subject'/><br />
+              <textarea name="Write Your Message Here" id=""></textarea><br />
+              <input type="submit" value="Submit Now"/>
+            </form>
+          </div>
+         </div>
+        </div>
+        <div className="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7064.609077968113!2d83.46243300000002!3d27.707882000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399687a9e810c4af%3A0x330398c480aee01b!2sPari%20Butwal!5e0!3m2!1sen!2snp!4v1753426254096!5m2!1sen!2snp" width="1200" height="400" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <footer>
+          <div className=""><p>Developed with love by Kristal &copy; 2025</p></div>
+          <div className=""><a href="">Terms And Conditions</a>&nbsp;<a href="">Privacy Policy</a></div>
+        </footer>
         </main>
         </>
   );
